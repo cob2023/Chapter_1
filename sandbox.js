2999544b3f0eac03fcbba2279efc8c6874914570
cob2023 <mocodcb@gmail.com>
2023-06-28 11:36:46 +0100

    for loop

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,11 @@
 //FOR LOOPS
-for(let i=0;i<5;i++){
+for(let i=0;i<=5;i++){
     console.log(i)
+}
+
+//transversing an array
+const names=['Pat', 'cillian','ciaran']
+
+for(let i=0;i<names.length;i++){
+    console.log(names[i])
 }
