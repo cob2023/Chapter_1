f8ea5bec4566515aa0faaf186a517cb642281ab9
cob2023 <mocodcb@gmail.com>
2023-06-30 13:21:22 +0100

    while loop

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,5 @@
 //FOR LOOPS
+/*
 for(let i=0;i<=5;i++){
     console.log(i)
 }
@@ -8,4 +9,17 @@ const names=['Pat', 'cillian','ciaran']
 
 for(let i=0;i<names.length;i++){
     console.log(names[i])
+}
+*/
+//while loops
+let counter=0;//initilises counter to 0
+while(counter<5){
+    console.log('Hi '+counter);
+    counter++;//increments counter
+}
+const names=['Pat', 'cillian','ciaran']
+let i=0;
+while (i<names.length){
+    console.log(names[i]);
+    i++;
 }
