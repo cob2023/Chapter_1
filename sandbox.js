78b3f4239cc5e49abe0b203567b30d51c9d915a8
cob2023 <mocodcb@gmail.com>
2023-07-02 11:03:23 +0100

    if else

--- sandbox.js
+++ sandbox.js
@@ -23,3 +23,36 @@ while (i<names.length){
     console.log(names[i]);
     i++;
 }
+
+//Do while loop
+let n=5;
+//code will always be executes oncs as the loop sentinel is the the end
+do{
+    console.log("the val of n is "+n);
+}
+while (n<5);
+//if loop
+let password='pass12345678';
+if(password.length<=8){
+    console.log("Password is too short");
+}
+else{
+    console.log('too long')
+
+}
+//age checker for driving licence 
+let age=prompt("plese enter you age. ");
+//condtional
+if (age<17){
+    console.log("Sorry too young");
+}
+else if (age>100){
+    console.log("Sorry too old");
+}
+else{
+    console.log("All ok");
+}
+
+
+
+
