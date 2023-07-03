572bed9ab0853253e65c3544a20c5fef118ba1ac
cob2023 <mocodcb@gmail.com>
2023-07-03 16:56:39 +0100

    test

---
+++ sandbox.js
@@ -0,0 +1,23 @@
+//alert("hi") ;  
+//strings
+console.log("this is a string");
+
+//concatenation
+
+console.log("this is a string"+" so am I.");
+let firstNAME="Clement";
+let secondName=" Byrnes";
+fullName= firstNAME+ secondName;
+console.log(fullName);
+
+//string methods
+myString="Clement";
+console.log(myString.toUpperCase());
+console.log(myString.toLowerCase());
+
+
+console.log(myString[2]);
+//string lenght
+
+myString="Clement";
+console.log(myString.length+"   "+ "char long");
